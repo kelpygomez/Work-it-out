8d7e2b3649f71756f911b7f33620be328cca99f1
Kelpy <keelpygomez@gmail.com>
2024-04-23 16:16:39 +0100

    34 Home page is now improved

--- work-it-out-ionic/src/app/pages/home/home.module.ts
+++ work-it-out-ionic/src/app/pages/home/home.module.ts
@@ -5,7 +5,6 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-import { FooterPageModule } from 'src/app/components/footer/footer.module';
 
 
 @NgModule({
