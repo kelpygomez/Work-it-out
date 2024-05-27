6ac871f37b2c863d6ed2876c92ab4e5b1dbc3886
Kelpy <keelpygomez@gmail.com>
2024-05-27 16:48:08 +0100

    changes

--- work-it-out-ionic/src/app/pages/routine-maker/routine-maker.page.ts
+++ work-it-out-ionic/src/app/pages/routine-maker/routine-maker.page.ts
@@ -13,16 +13,19 @@ import Swal from 'sweetalert2';
   styleUrls: ['./routine-maker.page.scss'],
 })
 export class RoutineMakerPage implements OnInit {
-  userId: number= 0;
+  userId: number = 0;
   routineId: number = 0;
   routine: Routine = { id: 0, name: '', total_kcal: 0, description: '', types: '', exercises: [] };
   availableExercises: Exercise[] = [];
   routineExercises: Exercise[] = [];
   filteredExercises: any[] = [];
   searchTerm: string = '';
+  isLoading: boolean = true;
+  selectedType!: string;
+  selectedMaterial!: string;
 
   constructor(private http: HttpClient, private route: ActivatedRoute, private routineService: RoutineService, private exerciseService: ExerciseService, private router: Router) { }
-  isLoading: boolean = true;
+
   ngOnInit() {
     setTimeout(() => {
       this.isLoading = false;
@@ -40,7 +43,6 @@ export class RoutineMakerPage implements OnInit {
       (data) => {
         this.userId = data.user_id;
         console.log('User ID:', this.userId);
-        // Aquí puedes hacer otras acciones utilizando el ID del usuario
       },
       (error) => {
         console.error('Error fetching user ID:', error);
@@ -63,7 +65,6 @@ export class RoutineMakerPage implements OnInit {
   loadExercises() {
     this.exerciseService.getExerciseList().subscribe(
       (data: Exercise[]) => {
-        // Filtrar ejercicios disponibles que no están en la rutina
         this.availableExercises = data.filter(exercise => !this.routine.exercises.find(e => e.id === exercise.id));
         this.filteredExercises = this.availableExercises;
       },
@@ -73,19 +74,20 @@ export class RoutineMakerPage implements OnInit {
     );
   }
 
+  toggleExercise(exercise: Exercise) {
+    if (this.isExerciseInRoutine(exercise.id)) {
+      this.removeExerciseFromRoutine(exercise.id);
+    } else {
+      this.addExerciseToRoutine(exercise.id);
+    }
+  }
+
   addExerciseToRoutine(exerciseId: number) {
     this.routineService.addExerciseToRoutine(this.routineId, exerciseId).subscribe(
       () => {
         this.saveChanges();
-        this.loadRoutine(); // Recargar rutina después de agregar ejercicio
-        this.loadExercises(); // Recargar ejercicios disponibles
-        Swal.fire({
-          title: 'Success!',
-          text: 'Exercise added to routine.',
-          icon: 'success',
-          confirmButtonColor: "#1d965b",
-
-        });
+        this.loadRoutine();
+        this.loadExercises();
       },
       (error: any) => {
         console.error('Error adding exercise to routine:', error);
@@ -97,25 +99,8 @@ export class RoutineMakerPage implements OnInit {
     this.routineService.removeExerciseFromRoutine(this.routineId, exerciseId).subscribe(
       () => {
         this.saveChanges();
-        this.loadRoutine(); // Recargar rutina después de quitar ejercicio
+        this.loadRoutine();
         this.loadExercises();
-        Swal.fire({
-          title: "Are you sure?",
-          icon: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#1d965b",
-          cancelButtonColor: "#d33",
-          confirmButtonText: "Yes, delete it!"
-        }).then((result) => {
-          if (result.isConfirmed) {
-            Swal.fire({
-              title: "Deleted!",
-              text: "The exercise has been deleted.",
-              icon: "success",
-              confirmButtonColor: "#1d965b",
-            });
-          }
-        });
       },
       (error: any) => {
         console.error('Error removing exercise from routine:', error);
@@ -123,50 +108,42 @@ export class RoutineMakerPage implements OnInit {
     );
   }
 
-    // Agrega un método para guardar los cambios en la rutina
-    saveChanges() {
-      const data = {
-        id: this.routine.id,
-        name: this.routine.name,
-        description: this.routine.description
-      };
-      this.routineService.updateRoutine(data).subscribe(
-        () => {
-          Swal.fire({
-            title: 'Success!',
-            text: 'Routine saved succesfully.',
-            icon: 'success',
-            confirmButtonColor: "#1d965b",
-          });
-          console.log('Changes saved successfully');
-        },
-        (error: any) => {
-          console.error('Error saving changes:', error);
-        }
-      );
-    }
+  saveChanges() {
+    const data = {
+      id: this.routine.id,
+      name: this.routine.name,
+      description: this.routine.description
+    };
+    this.routineService.updateRoutine(data).subscribe(
+      () => {
+        console.log('Changes saved successfully');
+      },
+      (error: any) => {
+        console.error('Error saving changes:', error);
+      }
+    );
+  }
 
-    filterExercises(searchTerm: string) {
-      this.searchTerm = searchTerm;
-      this.filteredExercises = this.availableExercises.filter(exercise =>
-        exercise.name.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
-        exercise.type.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
-        exercise.description.toLowerCase().includes(this.searchTerm.toLowerCase())
-      );
+  filterExercises(searchTerm: string) {
+    this.searchTerm = searchTerm;
+    this.filteredExercises = this.availableExercises.filter(exercise =>
+      exercise.name.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+      exercise.type.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+      exercise.description.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
+
+  filterExercisesSelect() {
+    this.filteredExercises = this.availableExercises;
+    if (this.selectedType) {
+      this.filteredExercises = this.filteredExercises.filter(exercise => exercise.type === this.selectedType);
     }
-  
-    selectedType!: string;
-    selectedMaterial!: string;
-    
-    filterExercisesSelect() {
-      this.filteredExercises = this.availableExercises;
-    
-      if (this.selectedType) {
-        this.filteredExercises = this.filteredExercises.filter(exercise => exercise.type === this.selectedType);
-      }
-    
-      if (this.selectedMaterial) {
-        this.filteredExercises = this.filteredExercises.filter(exercise => exercise.required_material === this.selectedMaterial);
-      }
+    if (this.selectedMaterial) {
+      this.filteredExercises = this.filteredExercises.filter(exercise => exercise.required_material === this.selectedMaterial);
     }
+  }
+
+  isExerciseInRoutine(exerciseId: number): boolean {
+    return !!this.routine.exercises.find(exercise => exercise.id === exerciseId);
+  }
 }
