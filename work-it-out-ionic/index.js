7dcc9fabbad2f0f41fd2256d9252aff299785951
Andres Gonzalez <andres.gonzalez.izquierdo@gmail.com>
2024-05-27 10:52:12 +0000

    chanegs

--- work-it-out-ionic/index.js
+++ work-it-out-ionic/index.js
@@ -5,6 +5,11 @@ const app = express();
 // Servir archivos estáticos de la carpeta 'www' (versión de producción)
 app.use(express.static(path.join(__dirname, 'www')));
 
+// Redirigir todas las rutas a index.html para que Angular pueda manejarlas
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'www', 'index.html'));
+});
+
 // Escuchar en el puerto 8100
 const PORT = process.env.PORT || 8100;
 app.listen(PORT, () => {
